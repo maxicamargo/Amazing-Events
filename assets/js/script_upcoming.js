09bfef77d12e22c153a4f7d8e71609a1c1dc9d54
Maximiliano Camargo <maxi.camargo@gmail.com>
2023-02-27 16:52:55 -0300

    Se configuro la carga dinamica de las card en las paginas home, past y upcomig

--- assets/js/script_upcoming.js
+++ assets/js/script_upcoming.js
@@ -6,16 +6,23 @@ for (let event of data.events)
     let currentDate = new Date(data.currentDate);
     let eventDate = new Date(event.date);
     if(eventDate>currentDate){
-        htmlEvents += `<div class="card-body text-center">
-        <img src="${event.image}" class="card_img card-img-top" alt="...">
-        <h5 class="card-title mt-2 fw-bold ">${event.name}</h5>
-        <p class="card-text">${event.description}</p>
-        <div class="d-flex justify-content-between mt-5">
-        <p>Price: $ ${event.price}
-        <p>
-            <button type="button" class="card_btn btn btn-outline-dark ">Ver mas</button>
+        htmlEvents += `<div class="col-sm-12 col-md-6 col-xl-3 py-2">
+        <div class="card">
+            <div class="card-body text-center">
+                <img src="${event.image}" class="card_img card-img-top" alt="...">
+                <h5 class="card-title mt-2 fw-bold ">${event.name}</h5>
+                <p class="card-text">${event.description}</p>
+                <div class="d-flex justify-content-between mt-5">
+                    <p>Price: $ ${event.price}
+                    <p>
+                    <a class="card_btn btn btn-outline-dark" href="./details.html" role="button">Ver mas</a>
+                </div>
+            </div>
         </div>
-        </div>`;
+    </div>`;
     }
 }
-console.log(htmlEvents);
+
+document.querySelector(".row").innerHTML=htmlEvents;
+
+// console.log(htmlEvents);
